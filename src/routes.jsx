6d42f73bee30aaf38b6a2e1e2e6a5997b1996b97
Mateus Oliveira <44128449865@escola928.edu>
2024-05-27 14:41:42 -0300

    commit

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Registro from "./pages/cadastro/RegistrationForm";
 import Login from "./pages/login/Login";
 import Turmas from "./pages/home/Turmas";
-import Idev2 from "./pages/turmas/Idev2";
+
+import Idev1 from "./pages/turmas/idev1/idev1";
+import Idev2 from "./pages/turmas/idev2/Idev2";
+import Idev3 from "./pages/turmas/idev3/Idev3";
+
+import Ilecmec1 from "./pages/turmas/ilecmec1/ilecmec1";
+import Ilecmec2 from "./pages/turmas/ilecmec2/ilecmec2";
+import Ilecmec3 from "./pages/turmas/ilecmec3/ilecmec3";
 
 function Rotas() {
     return (
@@ -11,7 +18,14 @@ function Rotas() {
                 <Route path="/" element={<Registro />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/turma" element={<Turmas />} />
+
+                <Route path="/idev1" element={<Idev1/>}/>
                 <Route path="/idev2" element={<Idev2 />} />
+                <Route path="/idev3" element={<Idev3/>}/>
+
+                <Route path="/ilecmec1" element={<Ilecmec1/>}/>
+                <Route path="/ilecmec2" element={<Ilecmec2 />} />
+                <Route path="/ilecmec3" element={<Ilecmec3/>}/>
             </Routes>
         </div>
     )
