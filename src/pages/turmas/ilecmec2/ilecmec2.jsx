df94a4b819f2e522bcf91636671585068b696239
Mateus Oliveira <44128449865@escola928.edu>
2024-05-27 14:49:40 -0300

    commit

---
+++ src/pages/turmas/ilecmec2/ilecmec2.jsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import './ilecmec2.css'; // Importe os estilos CSS
+
+function Container() {
+    return (
+        <>
+            <header>
+                <img src="./src/img/senaipreta.png" alt="logo senai" width={220} className='senaipreto' />
+                <h1 className='fodase'>IDEV2</h1>
+                <img src="./src/img/icon.png" alt="icon" width={50} className='icone'/>
+            </header>
+            <div className="geral-ficha1">
+                <div className='fundoDosUsuarios1'>
+                    <div className="container3">
+                        <div className='conteudo1'>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="./Ficha" className='nomes-ficha'>ALEXANDRE</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>CAMILLY</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>CAROL</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>CLAUDETE</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>EDUARDA</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>ENZO</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>FELIPE</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>GABRIEL O.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>GABRIEL P.</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>GABRIELLE</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>GIOVANI</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>GUILHERME F.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>GUILHERME T.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>HENRIQUE</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>HENRY</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>ISABELA</a>
+                            </div>
+                            <div className='imagem-container-nda-s'>
+                                <img src="./img/perfil.png" alt="" className='imagem1' />
+                                <a href="./Ficha-nda" className='nomes-ficha'>ISADORA</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>JHUAN</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>JUAN</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>KAUAN</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>LUCAS L.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>LUCAS M.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>LUCAS Z.</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>MANUELA</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>MATEUS</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>MATHEUS</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>PEDRO G.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>PEDRO H.</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="./Ficha" className='nomes-ficha'>TOMÁS</a>
+                            </div>
+                            <div className='imagem-container-masc-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>VINICIUS</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>VITÓRIA</a>
+                            </div>
+                            <div className='imagem-container-fem-s'>
+                                <img src="./img/usuario.png.png" alt="" className='imagem1' />
+                                <a href="#" className='nomes-ficha'>YASMIM</a>
+                            </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </>
+    );
+}
+
+export default Container;
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,13 +3,10 @@ import Registro from "./pages/cadastro/RegistrationForm";
 import Login from "./pages/login/Login";
 import Turmas from "./pages/home/Turmas";
 
-import Idev1 from "./pages/turmas/idev1/idev1";
 import Idev2 from "./pages/turmas/idev2/Idev2";
-import Idev3 from "./pages/turmas/idev3/Idev3";
 
-import Ilecmec1 from "./pages/turmas/ilecmec1/ilecmec1";
 import Ilecmec2 from "./pages/turmas/ilecmec2/ilecmec2";
-import Ilecmec3 from "./pages/turmas/ilecmec3/ilecmec3";
+
 
 function Rotas() {
     return (
@@ -19,13 +16,9 @@ function Rotas() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/turma" element={<Turmas />} />
 
-                <Route path="/idev1" element={<Idev1/>}/>
                 <Route path="/idev2" element={<Idev2 />} />
-                <Route path="/idev3" element={<Idev3/>}/>
 
-                <Route path="/ilecmec1" element={<Ilecmec1/>}/>
                 <Route path="/ilecmec2" element={<Ilecmec2 />} />
-                <Route path="/ilecmec3" element={<Ilecmec3/>}/>
             </Routes>
         </div>
     )
